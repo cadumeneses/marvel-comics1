f63c3aedff9c3737efe7999f1b3ab45bced88b69
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-08-15 09:52:03 -0300

    feat: add modal and nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { LayoutModule } from '@angular/cdk/layout'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListHqModule } from './modules/list-hq/list-hq.module';
+import { ListHqModule } from './modules/list-hq.module';
+import { ModalComponent } from './componentes/modal/modal.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CoreModule } from 'src/app/modules/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FlexLayoutModule,
+    LayoutModule,
     ListHqModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'commics', pathMatch: 'full'
